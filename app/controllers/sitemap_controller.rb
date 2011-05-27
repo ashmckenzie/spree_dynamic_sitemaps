ddbf05fdd17cf89cf5050fd72c3ae739d53779f6
Robert Sköld <robert@publicclass.se>
2011-05-27 14:03:26 +0200

    Added rejectable taxons, using routed urls for taxons and products.

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -1,9 +1,6 @@
 class SitemapController < Spree::BaseController
   def index
-    @public_dir = root_url
-    
-    @products = Product.active.find(:all)
-    @taxonomies = Taxonomy.all
+    @taxons = Taxon.all
     
     # Get Pages from static_content extension
     @pages = _select_static_pages
@@ -13,8 +10,7 @@ class SitemapController < Spree::BaseController
       format.xml do
         nav = _build_taxon_hash
         nav = _build_pages_hash nav
-        nav = _add_products_to_tax nav, false
-        render :layout => false, :xml => _build_xml(nav, @public_dir)
+        render :layout => false, :xml => _build_xml(nav)
       end
       format.text do
         @nav = _add_products_to_tax(_build_taxon_hash, false)
@@ -22,22 +18,31 @@ class SitemapController < Spree::BaseController
       end
     end
   end
-
+    
   private
-  def _build_xml(nav, public_dir)
+  
+  def taxons_to_reject
+    @taxons_to_reject ||= []
+  end
+  
+  def slugs_to_reject
+    @slugs_to_reject ||= []
+  end
+  
+  def _build_xml(nav)
     returning '' do |output|
       xml = Builder::XmlMarkup.new(:target => output, :indent => 2) 
       xml.instruct!  :xml, :version => "1.0", :encoding => "UTF-8"
       xml.urlset( :xmlns => "http://www.sitemaps.org/schemas/sitemap/0.9" ) {
         xml.url {
-          xml.loc public_dir
+          xml.loc root_url
           xml.lastmod Date.today
           xml.changefreq 'daily'
           xml.priority '1.0'
         }
         nav.each do |k, v| 
           xml.url {
-            xml.loc public_dir + v['link']
+            xml.loc v['link']
             xml.lastmod v['updated'].xmlschema			  #change timestamp of last modified
             xml.changefreq 'weekly'
             xml.priority '0.8'
@@ -49,24 +54,25 @@ class SitemapController < Spree::BaseController
 
   def _build_taxon_hash
     nav = Hash.new
-    Taxon.find(:all).each do |taxon|
+    @taxons.each do |taxon|
+      next if taxons_to_reject.any? {|r| taxon.permalink.match( r ) }
       tinfo = Hash.new
       tinfo['name'] = taxon.name
       tinfo['depth'] = taxon.permalink.split('/').size
-      tinfo['link'] = 't/' + taxon.permalink 
+      tinfo['link'] = taxons_url(taxon.permalink) 
       tinfo['updated'] = taxon.updated_at
+      _add_products_to_tax(nav, taxon)
       nav[taxon.permalink] = tinfo
     end
     nav
   end
 
-  def _add_products_to_tax(nav, multiples_allowed)
-    Product.active.find(:all).each do |product|
+  def _add_products_to_tax(nav, taxon)
+    taxon.active_products.each do |product|
       pinfo = Hash.new
       pinfo['name'] = product.name
-      pinfo['link'] = 'products/' + product.permalink	# primary
+      pinfo['link'] = product_url(:id => product.permalink, :taxon => taxon.permalink) # primary
       pinfo['updated'] = product.updated_at
-
       nav[pinfo['link']] = pinfo				# store primary
     end
     nav
@@ -77,7 +83,7 @@ class SitemapController < Spree::BaseController
     
     @pages.each do |page|
       nav[page.slug] = {'name' => page.title,
-                        'link' => page.slug.gsub(/^\//, ''),
+                        'link' => content_url(page.slug),
                         'updated' => page.updated_at}
     end
     nav
@@ -86,9 +92,9 @@ class SitemapController < Spree::BaseController
   def _select_static_pages
     pages = []
     begin
-      slugs_to_reject = ["/on-main-page"]
       Page.visible.each do |page|
-        pages << page unless slugs_to_reject.include?(page.slug)
+        next if slugs_to_reject.any? {|r| page.slug.match( r ) }
+        pages << page
       end
       
     rescue NameError
